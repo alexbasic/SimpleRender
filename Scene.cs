59faa264095133877d80fee97f74dcfac2082fcf
Aleksandr Vladimirovich <alex_basic@mail.ru>
2015-03-30 12:40:29 +0600

    Добавлены файлы

---
+++ Scene.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleRender
+{
+    public class Scene
+    {
+        public Camera Camera { get; set; }
+        public List<Object3D> Objects { get; set; }
+
+        public void Render(Bitmap image) 
+        {
+            Camera.Render(this, image);
+        }
+    }
+}
---
+++ Vertex.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleRender
+{
+    public class Vertex
+    {
+        public int Number { get; set; }
+        public Single X { get; set; }
+        public Single Y { get; set; }
+        public Single Z { get; set; }
+    }
+}
