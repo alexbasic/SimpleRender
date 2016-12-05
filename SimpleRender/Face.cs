7727bb71aad9ce559165fd0271d0280e8a698597
ugsk\NikonovAV <NikonovAV@ugsk.ru>
2016-12-05 17:59:35 +0500

    reorganize

---
+++ SimpleRender/Face.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleRender
+{
+    public class Face
+    {
+        public int Number { get; set; }
+        public int Vertex1 { get; set; }
+        public int Vertex2 { get; set; }
+        public int Vertex3 { get; set; }
+
+        public int TextureVertex1 { get; set; }
+        public int TextureVertex2 { get; set; }
+        public int TextureVertex3 { get; set; }
+
+        public int NormalVertex1 { get; set; }
+        public int NormalVertex2 { get; set; }
+        public int NormalVertex3 { get; set; }
+    }
+}
---
+++ SimpleRender/WaveForm.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleRender
+{
+    public class WaveForm
+    {
+        public static Object3D Load(string fileName)
+        {
+            Object3D result = new Object3D();
+            result.Vertices = new List<Vertex>();
+            result.Normals = new List<Vertex>();
+            result.TextureVertices = new List<Vertex>();
+            result.Faces = new List<Face>();
+            int verticiseCounter = 0;
+            int normalsCounter = 0;
+            int textureVerticiseCounter = 0;
+            int facesCounter = 0;
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine().TrimStart();
+                    string normalizedLine = System.Text.RegularExpressions.Regex.Replace(line, " +", " ");
+                    var newNumberFormatInfo = new System.Globalization.NumberFormatInfo();
+                    newNumberFormatInfo.NumberDecimalSeparator = ".";
+                    if (line.StartsWith("v"))
+                    {
+                        string[] vectorElements = normalizedLine.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                        ++verticiseCounter;
+                        result.Vertices.Add(new Vertex
+                        {
+                            Number = verticiseCounter,
+                            X = Single.Parse(vectorElements[1], newNumberFormatInfo),
+                            Y = Single.Parse(vectorElements[2], newNumberFormatInfo),
+                            Z = Single.Parse(vectorElements[3], newNumberFormatInfo)
+                        });
+                    }
+                    if (line.StartsWith("vt"))
+                    {
+                        string[] vectorElements = normalizedLine.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                        ++textureVerticiseCounter;
+                        result.TextureVertices.Add(new Vertex
+                        {
+                            Number = textureVerticiseCounter,
+                            X = Single.Parse(vectorElements[1], newNumberFormatInfo),
+                            Y = Single.Parse(vectorElements[2], newNumberFormatInfo),
+                            Z = Single.Parse(vectorElements[3], newNumberFormatInfo)
+                        });
+                    }
+                    if (line.StartsWith("vn"))
+                    {
+                        string[] vectorElements = normalizedLine.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                        ++normalsCounter;
+                        result.Normals.Add(new Vertex
+                        {
+                            Number = normalsCounter,
+                            X = Single.Parse(vectorElements[1], newNumberFormatInfo),
+                            Y = Single.Parse(vectorElements[2], newNumberFormatInfo),
+                            Z = Single.Parse(vectorElements[3], newNumberFormatInfo)
+                        });
+                    }
+                    if (line.StartsWith("f"))
+                    {
+                        string[] faceElements = normalizedLine.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                        string[] faceElementItems1 = faceElements[1].Split(new char[] { '/' }, StringSplitOptions.None);
+                        string[] faceElementItems2 = faceElements[2].Split(new char[] { '/' }, StringSplitOptions.None);
+                        string[] faceElementItems3 = faceElements[3].Split(new char[] { '/' }, StringSplitOptions.None);
+                        ++facesCounter;
+                        result.Faces.Add(new Face
+                        {
+                            Number = facesCounter,
+                            Vertex1 = int.Parse(faceElementItems1[0]),
+                            TextureVertex1 = int.Parse(faceElementItems1[1]),
+                            NormalVertex1 = int.Parse(faceElementItems1[2]),
+                            Vertex2 = int.Parse(faceElementItems2[0]),
+                            TextureVertex2 = int.Parse(faceElementItems2[1]),
+                            NormalVertex2 = int.Parse(faceElementItems2[2]),
+                            Vertex3 = int.Parse(faceElementItems3[0]),
+                            TextureVertex3 = int.Parse(faceElementItems3[1]),
+                            NormalVertex3 = int.Parse(faceElementItems3[2])
+                        });
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
