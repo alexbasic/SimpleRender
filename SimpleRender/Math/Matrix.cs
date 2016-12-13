7acded43e511d0586d77f969c1571ca06782cdf0
Aleksandr Vladimirovich <alex_basic@mail.ru>
2016-12-13 17:45:56 +0500

    in progress

--- SimpleRender/Math/Matrix.cs
+++ SimpleRender/Math/Matrix.cs
@@ -12,12 +12,21 @@ namespace SimpleRender.Math
     {
         private double[,] _array { get; set; }
 
+        public Matrix(params double[] array)
+        {
+            _array = new double[4, 4];
+            _array[0, 0] = array[0]; _array[1, 0] = array[1]; _array[2, 0] = array[2]; _array[3, 0] = array[3];
+            _array[0, 1] = array[4]; _array[1, 1] = array[5]; _array[2, 1] = array[6]; _array[3, 1] = array[7];
+            _array[0, 2] = array[8]; _array[1, 2] = array[9]; _array[2, 2] = array[10]; _array[3, 2] = array[11];
+            _array[0, 3] = array[12]; _array[1, 3] = array[13]; _array[2, 3] = array[14]; _array[3, 3] = array[15];
+        }
+
         public double this[int item]
         {
-            get { return _array[item / 4, item % 4]; }
+            get { return _array[ item % 4, item / 4]; }
             set
             {
-                _array[item / 4, item % 4] = value;
+                _array[item % 4, item / 4] = value;
             }
         }
 
@@ -25,11 +34,11 @@ namespace SimpleRender.Math
         {
             get
             {   
-                return _array[row, coll];
+                return _array[coll, row];
             }
             set
             {
-                _array[row, coll] = value;
+                _array[coll, row] = value;
             }
         }
     }
--- SimpleRender/SceneObjects/ICamera.cs
+++ SimpleRender/SceneObjects/ICamera.cs
@@ -61,5 +61,16 @@ namespace SimpleRender.SceneObjects
             //2*lp*System.Math.Tan(fov/2);
             return null;
         }
+
+        private Matrix GetFrustum(double left, double right, double bottom, double top, double near, double far)
+        {
+            var matrix = new Matrix(
+                2*near/(right-left), 0,                   (right+left)/(right-left), 0,
+                0,                   2*near/(top-bottom), (top+bottom)/(top-bottom), 0,
+                0,                   0,                   -1*(far+near)/(far-near),  -1*(2*far*near)/(far-near),
+                0,                   0,                   -1,                        0
+                );
+            return matrix;
+        }
     }
 }
