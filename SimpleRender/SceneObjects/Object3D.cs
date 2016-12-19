8276748ad81cb2f2a4612ad589496f540e63c6bd
NikonovAV <alex_basic@mail.ru>
2016-12-19 23:27:07 +0500

    in progress

--- SimpleRender/SceneObjects/Object3D.cs
+++ SimpleRender/SceneObjects/Object3D.cs
@@ -10,6 +10,7 @@ namespace SimpleRender.SceneObjects
     public class Object3D
     {
         public Vertex Position { get; set; }
+        public Vector3f Rotation { get; set; }
         public Vertex[] Vertices { get; set; }
         public ICollection<Vertex> TextureVertices { get; set; }
         public ICollection<Vertex> Normals { get; set; }
