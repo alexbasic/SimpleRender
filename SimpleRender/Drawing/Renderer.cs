35dd4f26a137b7ccc87a009abd02b345b3582265
NikonovAV <alex_basic@mail.ru>
2017-02-03 22:32:11 +0500

    tada

---
+++ SimpleRender/Drawing/Renderer.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using SimpleRender.Drawing;
+using SimpleRender.Drawing.Shaders;
+using SimpleRender.Math;
+
+namespace SimpleRender.SceneObjects
+{
+    public class Renderer
+    {
+        private double _halfScreenWidth;
+        private double _halfscreenHeight;
+
+        private int _screenWidth;
+        private int _screenHeight;
+
+        public Bitmap FrameBuffer { get; set; }
+        private double[] _zBuffer;
+
+        public IShaderProgram ShaderProgram { get; set; }
+
+        public Renderer(int screenWidth, int screenHeight)
+        {
+            _screenWidth = screenWidth;
+            _screenHeight = screenHeight;
+            _halfScreenWidth = (int)((screenWidth - 1) / 2);
+            _halfscreenHeight = (int)((screenHeight - 1) / 2);
+            FrameBuffer = new Bitmap(screenWidth, screenHeight);
+            _zBuffer = new double[_screenWidth * _screenHeight];
+        }
+
+        public void Render(Scene scene)
+        {
+            //TransformedVector = TranslationMatrix * RotationMatrix * ScaleMatrix * OriginalVector;
+            //MVPmatrix = projection * view * model; 
+
+            ClearBuffers();
+
+            var cvvMatrix = Math3D.GetPerspectiveMatrix(100, _halfScreenWidth / _halfscreenHeight, 0.3d, 1000d);
+            foreach (var primitive in scene.Objects)
+            {
+                var rotationMatrix = Math3D.GetRotationMatrix(
+                    primitive.Rotation.X,
+                    primitive.Rotation.Y,
+                    primitive.Rotation.Z);
+
+                var translationMatrix = Math3D.GetTranslationMatrix(primitive.Position.X, primitive.Position.Y, primitive.Position.Z);
+
+                var scaleMatrix = Math3D.GetScaleMatrix(1, 1, 1);
+                var modelMatrix = translationMatrix * (rotationMatrix * scaleMatrix);
+                var viewMatrix = Math3D.GetViewMatrix(new Vector3f(0f, 1.2f, -2f), new Vector3f(0, 0f, 0f));
+
+                var transformMatrix = cvvMatrix * viewMatrix;
+
+                ShaderProgram = new ShaderProgram(cvvMatrix, transformMatrix, modelMatrix, primitive.Mategial);
+
+                foreach (var triangle in primitive.Faces)
+                {
+                    DrawTriangle(
+                        primitive.Vertices[triangle.Vertex1],
+                        primitive.Vertices[triangle.Vertex2],
+                        primitive.Vertices[triangle.Vertex3]);
+                }
+            }
+        }
+
+        private void DrawTriangle(Vertex v1, Vertex v2, Vertex v3)
+        {
+            //var v1 = primitive.Vertices[triangle.Vertex1];
+            //var v2 = primitive.Vertices[triangle.Vertex2];
+            //var v3 = primitive.Vertices[triangle.Vertex3];
+
+            //TODO Здесь нужен вызов фрагментного шейдера
+            //shader.Set(v1,v2,v3) shader.Compute()
+
+            Vector3f faceNormal = Math3D.CalculateNormal(Math3D.ConvertToDecart(v1), Math3D.ConvertToDecart(v2), Math3D.ConvertToDecart(v3));
+
+            var worldCoord1 = _modelMatrix * new Vector4(v1.Position.X, v1.Position.Y, v1.Position.Z, 1);
+            var worldCoord2 = _modelMatrix * new Vector4(v2.X, v2.Y, v2.Z, 1);
+            var worldCoord3 = _modelMatrix * new Vector4(v3.X, v3.Y, v3.Z, 1);
+
+            ShaderProgram.ComputeVertex(new VertexInput{Position = v1, Normal = (Vector4)faceNormal});
+            ShaderProgram.ComputeVertex(vi2);
+            ShaderProgram.ComputeVertex(vi3);
+
+            Draw3D.SimpleRasterizeTriangle(
+                ConvertToScreenCoord0(decartvector1),
+                ConvertToScreenCoord0(decartvector2),
+                ConvertToScreenCoord0(decartvector3),
+                FrameBuffer,
+                Color.FromArgb((int)(255 * Restrict(sampleColor.X)), (int)(255 * Restrict(sampleColor.Y)), (int)(255 * Restrict(sampleColor.Z))),
+                _zBuffer);    
+        }
+
+        private void ClearBuffers()
+        {
+            var graphics = Graphics.FromImage(FrameBuffer);
+            graphics.Clear(Color.Black);
+
+            for (var x = 0; x < _zBuffer.Length; x++)
+            {
+                _zBuffer[x] = 0d;
+            }
+        }
+
+        private float Restrict(float x)
+        {
+            if (x > 1f) return 1f;
+            if (x < 0f) return 0f;
+            return x;
+        }
+
+        private Vector3f ConvertToScreenCoord0(Vector3f decart)
+        {
+            var screenX = _halfScreenWidth + _halfScreenWidth * decart.X;
+            var screenY = _halfscreenHeight - _halfscreenHeight * decart.Y;
+
+            return new Vector3f((float)screenX, (float)screenY, decart.Z);
+        }
+    }
+}
---
+++ SimpleRender/Drawing/Shaders/ShaderProgram.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleRender.Math;
+using SimpleRender.SceneObjects;
+
+namespace SimpleRender.Drawing.Shaders
+{
+    public abstract class PixelShader
+    {
+        //Output
+        public Vector4 OutColor { get; set; }
+        public double OutDepth { get; set; }
+
+        public abstract void Compute();
+    }
+
+    public abstract class VertexShader
+    {
+        //Output
+        public Vector4 Position { get; set; }
+        public Vector4 Color { get; set; }
+        public Vector2 TextCoord { get; set; }
+
+
+        public abstract void Compute();
+    }
+
+    public interface IShaderProgram
+    {
+        void ComputeVertex(VertexInput v1);
+        Fragment GetFragment();
+    }
+
+    public class ShaderProgram : IShaderProgram
+    {
+        public VertexShader VertexShader { get; set; }
+
+        public PixelShader PixelShader { get; set; }
+
+        private Matrix _cvvMatrix;
+        private Vector4 _ambientColor;
+        private Material _material;
+        private Matrix _transformMatrix;
+        private Matrix _modelMatrix;
+
+        public ShaderProgram(Matrix cvvMatrix, Matrix transformMatrix, Matrix modelMatrix, Material material)
+        {
+            _cvvMatrix = cvvMatrix;
+            _ambientColor = material.AmbientColor;
+            _material = material;
+            _transformMatrix = transformMatrix;
+            _modelMatrix = modelMatrix;
+        }
+
+        public void ComputeVertex(VertexInput v1)
+        {
+            #region Завернуть в шейдер
+
+            var worldCoord1 = _modelMatrix * new Vector4(v1.Position.X, v1.Position.Y, v1.Position.Z, 1);
+
+            var vector1 = _transformMatrix * worldCoord1;
+            //var vector2 = _transformMatrix * worldCoord2;
+            //var vector3 = _transformMatrix * worldCoord3;
+
+            var decartvector1 = Math3D.ConvertToDecart(vector1);
+            //var decartvector2 = Math3D.ConvertToDecart(vector2);
+            //var decartvector3 = Math3D.ConvertToDecart(vector3);
+
+            //Vector3f faceNormalInWorldCoord = Math3D.CalculateNormal(Math3D.ConvertToDecart(worldCoord1), Math3D.ConvertToDecart(worldCoord2), Math3D.ConvertToDecart(worldCoord3));
+
+            Vector3f faceNormalInProjectionCoord = SimpleRender.Math.Vector3f.CrossProductLeft((decartvector3 - decartvector1), (decartvector2 - decartvector1));
+            faceNormalInProjectionCoord = faceNormalInProjectionCoord.Normalize();
+            var viewDirection = new Vector4(0, 0, -1, 1);
+            double intensity = Math3D.DotProduct(faceNormalInProjectionCoord, viewDirection);
+
+            //TODO Подумать нужноли это в шейдере
+            if (intensity <= 0) continue;
+
+            //ambient = Ka,
+            //diffuse = Kd * cos(N, L),
+            //specular = Ks * pow(cos(R, V), Ns),
+            //intensity = ambient + amp * (diffuse + specular).
+
+            //-----------------
+            //http://www.gamedev.ru/code/articles/HLSL?page=4
+            //Lighting:
+            //Lambert (ambient lighting)
+            //Diffuse (diffuse lighting model)
+            //Phong (specular lighting model), Blinn (blinn specular lighting model)
+            //Sum of this
+
+            //Реалистичное освещение на основе Кука-Торренса
+
+            //-------------
+
+            var ligthSource = scene.LightSources.First();
+            var globalLightPosition = ligthSource.Position.Normalize();
+
+            double illuminationIntensity = Math3D.DotProduct(faceNormalInWorldCoord, globalLightPosition);
+            var diffuseColor = new Vector4(
+                                   _material.DiffuseColor.X * ligthSource.Color.X,
+                                   _material.Mategial.DiffuseColor.Y * ligthSource.Color.Y,
+                                   _material.Mategial.DiffuseColor.Z * ligthSource.Color.Z,
+                                   1)
+                               * illuminationIntensity;
+
+            //var reflection = (Vector3f.CrossProductLeft(faceNormalInWorldCoord ,
+            //    (Vector3f.CrossProductLeft(faceNormalInWorldCoord ,globalLightPosition) * 2.0f)) -
+            //    globalLightPosition).Normalize();   // reflected light
+
+            var sampleColor = _ambientColor + diffuseColor * ligthSource.Intensity;
+
+            #endregion
+        }
+
+        public Fragment GetFragment()
+        {
+            return new Fragment();
+        }
+    }
+
+    public struct VertexInput
+    {
+        public Vector4 Position { get; set; }
+        public Vector4 Normal { get; set; }
+        public Vector2 TextCoord { get; set; }
+    }
+
+    public struct Fragment
+    {
+        public Vector4 Color { get; set; }
+        public double Depth { get; set; }
+    }
+
+    //Возможная реализация билинейной фильтрации текстурной выборки 
+    //function tex2D(Sampler tex,vector2 Tcoord)
+    //{
+    //    color1=TextureBuffer[int(Tcoord.x*tex.Width)][int(Tcoord.y*tex.Height)];
+    //    color2=TextureBuffer[int(Tcoord.x*tex.Width)+1][int(Tcoord.y*tex.Height)];
+    //    color3=TextureBuffer[int(Tcoord.x*tex.Width)+okrug(Tcoord.x*tex.Width-tex.Width)][int(Tcoord.y*tex.Height)+
+    //    +okrug(Tcoord.y*tex.Height-tex.Height)];
+
+    //    return pixelOut=interpolateBiLine(color1,color2,color3,Tcoord);
+    //};
+}
